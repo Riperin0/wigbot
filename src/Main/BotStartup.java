0309aeba53fbc37808d18bb313c94b26b14d6fc4
jayy00 <62804401+jayy00@users.noreply.github.com>
2022-01-16 00:48:21 -0500

    more testing

--- src/Main/BotStartup.java
+++ src/Main/BotStartup.java
@@ -9,12 +9,23 @@ import net.dv8tion.jda.api.entities.Activity;
 public class BotStartup {
 	static String prefix = new Commands().prefix;
 	
+	
+	
+	
 	static String token = new token().token;
 	public static void main(String[] args) throws LoginException {
+		
+		System.out.print(new Commands().wiggy.p1.toString());
+		
+		
 		JDABuilder jda = JDABuilder.createDefault(token);
 		jda.setActivity(Activity.listening(prefix+"help"));
 		jda.setStatus(OnlineStatus.ONLINE);
 		jda.addEventListeners(new Commands());
 		jda.build();
 	}
+
+	
+	
+	
 }
